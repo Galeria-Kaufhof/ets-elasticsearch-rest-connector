89be5cb37d3d10950178f475c4d522b40eb7d938
Laszlo Lueck <laszlo.lueck@kaufhof.de>
2018-06-20 21:39:11 +0200

    fix issues

--- ets-elasticsearch-rest-connector-core/src/main/scala/de/kaufhof/ets/elasticsearchrestconnector/core/client/model/QueryObject.scala
+++ ets-elasticsearch-rest-connector-core/src/main/scala/de/kaufhof/ets/elasticsearchrestconnector/core/client/model/QueryObject.scala
@@ -3,7 +3,7 @@ package de.kaufhof.ets.elasticsearchrestconnector.core.client.model
 import de.kaufhof.ets.elasticsearchrestconnector.core.client.model.aggregations.query.{Aggregation, ChildAggregation}
 import de.kaufhof.ets.elasticsearchrestconnector.core.client.model.querySources.{BaseSourceBoolean, QueryBaseSource}
 import de.kaufhof.ets.elasticsearchrestconnector.core.client.model.queryTypes.Query
-import play.api.libs.json.{JsObject, Json, OWrites}
+import play.api.libs.json._
 
 case class QueryObject(
                         from: QueryFrom = QueryFrom(),
@@ -20,6 +20,19 @@ case class QueryObject(
 }
 
 object QueryObject {
-  implicit val writes: OWrites[QueryObject] = Json.writes[QueryObject]
+implicit val writes: OWrites[QueryObject] = new OWrites[QueryObject] {
+  override def writes(o: QueryObject): JsObject = {
+    JsObject(
+      Seq(
+        Some("from" -> JsNumber(o.from.value)),
+        Some("size" -> JsNumber(o.size.value)),
+        Some("_source" -> QueryBaseSource.writes.writes(o._source)),
+        o.aggregations.map(aggregation => "aggregations" -> Aggregation.writes.writes(aggregation)),
+        o.sort.map(so => "sort" -> JsArray(so.map(s =>Sort.writes.writes(s)))),
+        Some("query" -> Query.writes.writes(o.query))
+      ).flatten
+    )
+  }
+}
 }
 
